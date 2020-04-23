159e96d3ac0258e10899b6463484f097bd10a017
m-r-poole-dev <55984889+m-r-poole-dev@users.noreply.github.com>
2020-04-23 14:42:41 +0100

    mobile_version_complete

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 window.addEventListener("load", (evt) =>{
   var showMenu = false;
   domStrings = {
-    hamburger: document.querySelector('.nav-img'),
+    hamburger: document.querySelector('#hamburger'),
+    close: document.querySelector('#close'),
     nav: document.querySelector("nav"),
     topSection: document.querySelector(".top-section"),
     mockups: document.querySelector('#mockups'),
@@ -12,18 +13,25 @@ window.addEventListener("load", (evt) =>{
 
   domStrings.hamburger.addEventListener("click", (evt)=>{
       if (!showMenu){
+          domStrings.hamburger.style.display = "none";
+          domStrings.close.style.display = "block";
           domStrings.nav.style.display = "block";
           domStrings.mockups.style.display="none";
           domStrings.topSection.classList.add("fade-top-section");
           domStrings.intro.classList.add("hide-intro");
           showMenu =true;
         }
-        else{
-          domStrings.nav.style.display = "none";
-          domStrings.mockups.style.display="inline-block";
-          domStrings.topSection.classList.remove("fade-top-section");
-          domStrings.intro.classList.remove("hide-intro");
-          showMenu =false;
-        }
   });
+
+  domStrings.close.addEventListener("click", (evt)=>{
+    if(showMenu){
+      domStrings.hamburger.style.display = "block";
+      domStrings.close.style.display = "none";
+      domStrings.nav.style.display = "none";
+      domStrings.mockups.style.display="inline-block";
+      domStrings.topSection.classList.remove("fade-top-section");
+      domStrings.intro.classList.remove("hide-intro");
+      showMenu =false;
+    }
+});
 });
